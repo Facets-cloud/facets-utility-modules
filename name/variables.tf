7f41c01e037e57ca5943692a75eb6d88b33f4d4b
ishaankalra <ishaan.kalra@facets.cloud>
2025-01-27 20:48:47 +0530

    Fix grammatical errors in variables.tf descriptions

--- name/variables.tf
+++ name/variables.tf
@@ -1,22 +1,22 @@
 variable "resource_type" {
   type = string
-  description = "Resource type to be used while generating the name"
+  description = "Resource type to be used while generating the name."
 }
 
 variable "resource_name" {
   type = string
-  description = "Resource name to be used while generating the name"
+  description = "Resource name to be used while generating the name."
 }
 
 variable "is_k8s" {
   type = bool
   default = false
-  description = "Boolean flag to determine whether the resource is getting deployed in k8s or not. In case this is false, then the generated name will not have the project name within it."
+  description = "Boolean flag to determine whether the resource is getting deployed in Kubernetes. If false, the generated name will not contain the project name."
 }
 
 variable "limit" {
   type = number
-  description = "The maximum limit of the resource name to be generated. In case, if the generated name exceeds the limits, then a few hashing techniques will be used to reduce the character limit of the name."
+  description = "The maximum limit for the resource name to be generated. If the generated name exceeds the limit, hashing techniques will be used to reduce the character count."
 }
 
 variable "environment" {
@@ -27,11 +27,11 @@ variable "environment" {
 variable "globally_unique" {
   type = bool
   default = false
-  description = "Optional flag to determine whether the resource name to be generated has to be globally unique across any cloud."
+  description = "Optional flag to determine whether the generated resource name needs to be globally unique across all clouds."
 }
 
 variable "prefix" {
   type = string
   default = ""
-  description = "Optional flag to pass any prefix to the resource name generated. Useful in the cases if the generated name contains a integer in the beginning, and the naming convention does not allow any numeric value in the beginning."
+  description = "Optional flag to pass any prefix to the resource name. Useful if the generated name contains a number at the beginning, which may not comply with naming conventions."
 }
